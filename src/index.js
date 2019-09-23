370b787fb501eefb8943bb8dcc75d8a2ae55ba66
Alex Sadikov <sav1982@inbox.ru>
2019-09-23 22:50:06 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = [];
+    let words = [];
+    let string = "";
+    
+    for(let i = 0; i < expr.length; i += 10 ){
+        words.push(expr.substring(i, i+10));   
+    }
+    words.forEach((item) =>{
+        let str1 = null;
+        let str2 = "";
+        for(i = 0; i < item.length; i += 2){
+            str1 = item.substring(i, i + 2);
+            if (str1 === "00"){
+                continue;
+            } else if(str1 === "10") {
+                str2 += ".";
+            } else if(str1 === "11") {
+                str2 += "-";
+            }
+        }
+        letters.push(str2)
+    });
+    for(i = 0; i < letters.length; i++){
+        if(letters[i] === ""){
+            string += " ";
+        } else if(letters[i] in MORSE_TABLE){
+            string += MORSE_TABLE[letters[i]];
+        }
+    }
+    return string;
 }
 
 module.exports = {
